b31b8622929d28791c5fa683b34b02c23fa04851
Babajide <akindelebabajidev@gmail.com>
2026-02-14 21:06:31 +0000

    Delete terraform/install_nginx.sh

--- terraform/install_nginx.sh
+++
@@ -1,8 +0,0 @@
-  # This script runs on the first boot
-              #!/bin/bash
-              dnf update -y
-              dnf install -y nginx
-              systemctl start nginx
-              systemctl enable nginx
-              echo "<h1>Look to your right, now your in the mix !!!</h1>" > /usr/share/nginx/html/index.html
-              EOF
